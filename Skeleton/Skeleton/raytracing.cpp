ebb2acd6f9d6967d761eaebaa3dd71b31508c83e
marcell-szabo <61230352+marcell-szabo@users.noreply.github.com>
2021-04-13 17:40:12 +0200

    Update raytracing.cpp

--- Skeleton/Skeleton/raytracing.cpp
+++ Skeleton/Skeleton/raytracing.cpp
@@ -1,5 +1,5 @@
 //=============================================================================================
-// Mintaprogram: Zöld háromszög. Ervenyes 2019. osztol.
+// Mintaprogram: ZÃ¶ld hÃ¡romszÃ¶g. Ervenyes 2019. osztol.
 //
 // A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat, BOM kihuzando.
 // Tilos:
@@ -18,8 +18,6 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : Szabo Marcell
-// Neptun : LAUPQQ
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
